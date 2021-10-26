104fcd19a58bbecdc824f5c880dda497a1d7f1f2
GeovanneAtanazio <geovanne.atanazio@dcomp.ufs.br>
2021-10-26 05:23:17 -0300

    Comentando as funções da regra de negócios

--- app/Models/CargaHoraria.php
+++ app/Models/CargaHoraria.php
@@ -8,11 +8,24 @@ use App\Models\Entities\Certificado;
 
 class CargaHoraria
 {
+    /**
+     * Calcula a porcentagem da carga horaria complementar já cursa pelo aluno.
+     *
+     * @param  User  $aluno
+     * @return bool
+     */
     public static function cargaHorariaComplementarCursada(User $aluno)
     {
         return ((($aluno->carga_horaria_complementar/$aluno->curso->carga_horaria_complementar)*100));
     }
 
+    /**
+     * Atualiza a carga horaria complementar já cursa pelo aluno.
+     *
+     * @param  User $aluno
+     * @param  array $certificados
+     * @return bool
+     */
     public static function gerenciaCargaHoraria(User $aluno, $certificados)
     {
 
@@ -22,6 +35,12 @@ class CargaHoraria
 
     }
 
+    /**
+     * Calcula o somatorio dos certificados homologados.
+     *
+     * @param  array $certificados
+     * @return int
+     */
     public static function somatorioCertificados($certificados)
     {
         $cargaHorariaCertificados = 0;
@@ -33,7 +52,14 @@ class CargaHoraria
         }
         return $cargaHorariaCertificados;
     }
-
+    /**
+     * Compara a carga horária do aluno com a carga horária do curso dele,
+     * verificando se a primeira é maior que a segunda.
+     *
+     * @param  int $cargaHorariaAluno
+     * @param  int $cargaHorariaCurso
+     * @return bool
+     */
     public static function comparadorCargaHorariaCurso($cargaHorariaAluno, $cargaHorariaCurso)
     {
         if(($cargaHorariaAluno)>=($cargaHorariaCurso)){
